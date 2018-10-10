e35c59e89c92f089a82e4779bd1fe57765c4767c
p-ton-cheese <developementist@gmail.com>
2018-10-10 18:45:25 +0900

    test2

---
+++ practice02/src/practice02/diamond.java
@@ -0,0 +1,66 @@
+package practice02;
+
+public class diamond {
+	
+	void print2console() {
+		// 다이아몬드 출력을 위한 변수 선언.
+		int iter3, iter4, iter5, iter6;
+		
+		System.out.println("");
+		System.out.println("다이아몬드 출력");
+		System.out.println("");
+		
+		// *표시로 다이아 몬드 모양 만들기.
+		//     *
+		//    ***
+		//   *****
+		//  *******
+		// *********
+		//  *******
+		//   *****
+		//    ***
+		//     *
+		
+		// 모양을 표시하기 위해서 어떤 방법을 사용할지에 대한 고민 필요.
+		// 맨 윗줄을 *를 기준으로 세로 구분 하여 뒤쪽에 * 가 추가로 찍히는 부분을 구분.
+		// 가운데 공백이 없는 라인을 기준으로 위, 아래로 구분.
+		
+		// 1차 줄바꿈을 위한 for문 - 5번째 줄까지 표현
+		for (iter3 = 0; iter3 < 5; iter3++) {
+			// 2차 앞쪽의 공백을 표기하기 위한 for문 - 공백의 갯수가 4 ~ 0 으로 감소. 
+			for (iter4 = 4 - iter3; iter4 > 0; iter4--) {
+				System.out.printf(" ");
+			}
+			// 2차 * 모양을 표시하기 위한 for문 - 앞쪽 *의 갯수가 1 ~ 5 로 증가.
+			for (iter5 = iter3; iter5 >= 0; iter5--) {
+				System.out.printf("*");
+			}
+			// 2차 * 모양을 표기하기 위한 for문. - 뒤쪽 *의 갯수가 0 ~ 4 로 증가.
+			for (iter6 = 0; iter6 < iter3; iter6++) {
+				System.out.printf("*");
+			}
+			// 줄바꿈.
+			System.out.println("");
+		}
+		
+		// 1차 줄바꿈을 위한 for문. - 6 ~ 9번째 줄까지 표현
+		for(iter3 = 1; iter3 < 5; iter3++) {
+			// 2차 앞쪽의 공백을 표기하기 위한 for문. 공백의 갯수가 1 ~ 4으로 증가
+			for (iter4 = 0; iter4 < iter3; iter4++) {
+				System.out.printf(" ");
+			}
+			// 2차 * 모양을 표기하기 위한 for문. - 앞쪽 *의 갯수가 4 ~ 1로 감소.
+			for (iter5 = 5; iter5 > iter3; iter5--) {
+				System.out.printf("*");
+			}
+			// 2차 * 모양을 표기하기 위한 for문. - 뒤쪽 *의 갯수가 3 ~ 0 으로 감소.
+			for (iter6 = 4; iter6 > iter3; iter6--) {
+				System.out.printf("*");
+			}
+			// 줄바꿈.
+			System.out.println("");
+		}
+		System.out.println("");
+		System.out.println("");
+	}
+}
---
+++ practice02/src/practice02/sungjeock.java
@@ -0,0 +1,99 @@
+package practice02;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class sungjeock {
+
+	void print2console() throws IOException {
+		// 점수 입력의 안내를 위한 출력.		
+		System.out.println("점수 5개 입력");
+		
+		// 시스템에 입력을 하기 위한 변수 선언.
+		Scanner input = new Scanner(System.in);
+		
+		// 입력 받은 횟수
+		int iter = 1;
+		// 파일에서 값을 불러올때 정수로 변환시 값을 받을 변수.
+		int iScore; 
+		// 파일에 저장하기 위한 문자열 변수
+		String sIn;
+		// 파일에서 값을 불러 가지고 있을 문자열 변수.
+		String sOut;
+		
+		// 파일 관련 변수 선언 및 파일 이름 선언.
+		File file = new File("test.txt");
+		
+		// 파일에 저장하기 위한 객체 선언.
+        FileWriter writer = null;		
+
+        // 파일에 저장하기 위한 객체에 저장 위치 파일 관련 변수를 넣음.
+        writer = new FileWriter(file, false);
+        
+        // 성적을 입력 받기 위한 반복문.
+		while (iter < 6) {
+			// 성적 입력이 몇번째 인지 알려주기 위한 출력문.
+			System.out.printf("성적입력 %d : ", iter);
+			// 입력된 데이터를 문자열로 가져옴.
+			sIn = input.nextLine();
+			// 입력 받은 데이터에 줄바꿈 문자를 추가하여 버퍼에 쓰기. 
+			writer.write(sIn +"\r\n");
+			// 버퍼를 비우고 파일에 쓰기.
+	        writer.flush();
+	        	        
+	        // 다음 성적을 받거나 while문을 빠져나가기 위한 변수 값 증가.
+	        iter++;	        
+		}
+		
+		System.out.println("파일닫기");
+		writer.close();
+		
+		// 파일에서 값을 읽어오기 위한 객체 선언.
+		BufferedReader bReader = null;
+		
+		System.out.println("\n 성적 파일 열기 ");
+		// 파일 객체를 파일읽기 객체에서 생성한것을 다시 버퍼에서 읽어올수 있도록 할당.
+        bReader = new BufferedReader(new FileReader(file));
+        
+        // 파일에서 읽어온 점수 갯수 파악.
+        iter = 1;
+        
+        // 파일에서 더이상 읽어올 수 없을 때까지 반복, 1개 라인씩 읽어옴.
+        while((sOut = bReader.readLine()) != null) {
+        	// 버퍼에서 읽어온 문자열을 console에 출력.
+            System.out.printf("%s : ", sOut);
+            
+            // 성적을 숫자료 변환.
+            iScore = Integer.parseInt(sOut);
+            
+            System.out.printf("파일에서 읽어온 %d번째 ", iter);
+            
+            // 성적에 맞는 조건문을 실행.
+            if ( iScore > 90) {
+    			System.out.println("성적은 A입니다.");
+    		}
+    		else if (iScore > 80) {
+    			System.out.println("성적은 B입니다");
+    		}
+    		else if (iScore > 70) {
+    			System.out.println("성적은 C입니다");
+    		}
+    		else if (iScore > 60) {
+    			System.out.println("성적은 D입니다.");
+    		}
+    		else {
+    			System.out.println("성적은 F입니다.");
+    		}
+            
+            //다음 데이터를 위해 증가.
+            iter++;
+        }
+        
+        // 파일을 다 읽어왓으면 bufferedreader, filereader를 닫음.        
+        bReader.close();   
+	}
+}
